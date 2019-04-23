1700d224eac55ce86c06cec74f403e4bf9a17550
raptor72 <mctaras51@yandex.ru>
2019-04-23 09:46:14 +0400

    Add requirements

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-apihelper_proxy = {'https':'socks5://userid34Jm:1b081Is@185.20.184.42:1259'}
+apihelper_proxy = {'https':'socks5://user:passwd@180.20.20.20:1234'}
 
-grafana_token = {'Authorization': 'Bearer eyJrIjoiUHBiaU1LcDNCVTJERkJzMlZLSzJlSXRlYmt5WkVUa2siLCJuIjoidGVzdDIiLCJpZCI6MX0='}
+grafana_token = {'Authorization': 'Bearer eyJrIjoiUHBiaU1LcDNCVTJQRkJzMlZLTzJlSXRlGmt5WkVUa2siKCJuIjoidGVzdDIiLCJpZCI6MX0='}
 
-image_path = '/home/maksim/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-TlgBot/image/'
+image_path = '/home/user/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-TlgBot/image/'
 
 grafana_url = 'http://192.168.231.6:3000'
 
-bot_token = '521125979:AAHxpkPhMRA-29bRrgyXGigJeB7U5NcOFJA'
+bot_token = '732375761:AAGNBmRWGDyIZb-i9evBbZ1N9JJqNoQxjdE'
 
-user_list = [54570553, 54570554]
+user_list = [50000000, 276548653]
 
 check = 1
 
@@ -16,7 +16,7 @@ command_list = ['start', 'Stop', 'List', 'Pwd12',
                 'bot-testing-dashboard', 'Carbon agents localdomain', 'Metric recieved',
                 'open-dashboard', 'Commited points', 'Memussage', 'go back']
 
-logfile = '/home/maksim/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-TlgBot/log.txt'
+logfile = '/home/user/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-TlgBot/log.txt'
 
-admin_id = 54570553
+admin_id = 50000000
 
