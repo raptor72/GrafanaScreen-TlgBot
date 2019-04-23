d25621899f2775de496840529b44b75608ff463e
raptor72 <mctaras51@yandex.ru>
2019-04-23 10:09:35 +0400

    Change some example in config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+#example config
 apihelper_proxy = {'https':'socks5://user:passwd@180.20.20.20:1234'}
 
 grafana_token = {'Authorization': 'Bearer eyJrIjoiUHBiaU1LcDNCVTJQRkJzMlZLTzJlSXRlGmt5WkVUa2siKCJuIjoidGVzdDIiLCJpZCI6MX0='}
@@ -6,7 +7,7 @@ image_path = '/home/user/PycharmProjects/GrafanaScreen-TlgBot/app/GrafanaScreen-
 
 grafana_url = 'http://192.168.231.6:3000'
 
-bot_token = '732375761:AAGNBmRWGDyIZb-i9evBbZ1N9JJqNoQxjdE'
+bot_token = '000000000:AAGNBmRWGDyIZb-i9evBbZ1N9JJqNoQxjdE'
 
 user_list = [50000000, 276548653]
 
